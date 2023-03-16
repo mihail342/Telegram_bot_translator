9757577d36b0a39cf127d07d577a432bd79d3660
mihail342 <125372257+mihail342@users.noreply.github.com>
2023-03-16 22:37:01 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,54 @@
+# Бот переводчик, переводит с русского на английский и со всех на русский.
+# Библиотеки (pip install googletrans==3.1.0a0 , pip install pyTelegramBotAPI).
+
+## Импорт библиотек
+from googletrans import Translator
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+
+# Апи бота
+bot = AsyncTeleBot("API_bot", parse_mode=None)
+
+# Обработка команды /start приветствие.
+@bot.message_handler(commands=['start'])
+async def send_welcome(message):
+    await bot.reply_to(message,'------\n'
+                 + 'Здравствуй, '
+                 + message.from_user.first_name
+                 + ' \nПереведу с русского на английский \nИ с других языков на русский '
+                 +'\n------')
+
+# Обработка команды /help.
+@bot.message_handler(commands=['help'])
+async def send_welcome(message):
+    await bot.reply_to(message,'------\n'
+                 + 'Просто вводи текст и нажимай отправить\n'
+                 + 'Я сам определю какой это язык\n'
+                 + 'Если не перевел, попробуй еще раз\n'
+                 + 'Перевод гугл'
+                 +'\n------')
+
+# Обработка текста сообщения, если ввод на русском, то перевод на английский,
+# если другой язык, то перевод на русский.
+@bot.message_handler()
+async def user_text(message):
+    translator = Translator()
+
+    # Определение языка ввода.
+    lang = translator.detect(message.text)
+    lang = lang.lang
+
+    # Если ввод по русски, то перевести на английский по умолчанию.
+    if lang == 'ru':
+        send = translator.translate(message.text)
+        await bot.reply_to(message, '------\n'+ send.text +'\n------')
+
+    # Иначе другой язык перевести на русский {dest='ru'}.
+    else:
+        send = translator.translate(message.text, dest='ru')
+        await bot.reply_to(message, '------\n'+ send.text +'\n------')
+
+
+# Запуск и повторение запуска при сбое
+asyncio.run(bot.infinity_polling())
+
