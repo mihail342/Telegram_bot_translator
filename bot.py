7d0d005603f8365bd1616282725522d1c817ee3b
mihail342 <125372257+mihail342@users.noreply.github.com>
2023-03-17 02:55:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # Бот переводчик, переводит с русского на английский и со всех на русский.
-# Библиотеки (pip install googletrans==3.1.0a0  -важно утановить именно эту версию библиотеки,
+# Библиотеки (sudo apt install pip
+#             pip install googletrans==3.1.0a0  -важно утановить именно эту версию библиотеки,
 #             pip install pyTelegramBotAPI,
 #             pip install asyncio
 #             pip install aiohttp).
